306f5e8f87f77a38d5acd5a47d9e6d111856a99f
sophiamrobles <srobles2@hawk.iit.edu>
2022-11-22 17:05:13 -0600

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,6 +1,5 @@
 function initMap() {
-    var coordinates = {lat:41.900626660924985, lng:-87.62586197929068};
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom:10, center: coordinates});
-    var marker = new google.maps.Marker({position: coordinates, map:map});
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.6417352411086!2d-87.62590759999999!3d41.9005614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd35153dc599d%3A0xee4e0d7d58a14c5c!2sCHANEL!5e0!3m2!1sen!2sus!4v1669158181063!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 }
+
+
