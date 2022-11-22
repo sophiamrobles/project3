fb7423bbcc448a3483e5fc19d1e53853dd399c6f
sophiamrobles <srobles2@hawk.iit.edu>
2022-11-21 23:51:11 -0600

    fix error

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 function initMap() {
     var coordinates = {lat:41.900626660924985, lng:-87.62586197929068};
     var map = new google.maps.Map(
-        document.getElementById("map"), {zoom:10, center: coordinates});
+        document.getElementById('map'), {zoom:10, center: coordinates});
     var marker = new google.maps.Marker({position: coordinates, map:map});
 }
 window.initMap = initMap;
