74bc098cc85d22e46426cd61b01a38fa930399f3
sophiamrobles <srobles2@hawk.iit.edu>
2022-11-22 23:33:33 -0500

    adjustments

--- js/map.js
+++ js/map.js
@@ -1,12 +1,8 @@
 function initMap() {
-    var map = new google.maps.Map(
-        document.getElementById('map'), {center: {lat:41.900626660924985, lng:-87.62586197929068}, zoom:10,});
-    var marker = new google.maps.Marker({position: {lat:41.900626660924985, lng:-87.62586197929068}, map:map,});
+    var map = new google.maps.Map(document.getElementById("map"), {zoom: 10, center: {lat:41.900626660924985, lng:-87.62586197929068}, });
+    var marker = new google.maps.Marker({position: {lat:41.900626660924985, lng:-87.62586197929068}, map: map,});
     var infowindow = new google.maps.InfoWindow({
-        content:"I love this store"});
+      content:"I love this store" });
     infowindow.open(map, marker);
-}
-window.initMap = initMap;
-
-
-
+  }
+  window.initMap = initMap;
