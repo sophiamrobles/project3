06b78d4cf56c81a508bc27acf8a42f6f871e241d
sophiamrobles <srobles2@hawk.iit.edu>
2022-11-22 23:42:56 -0500

    fix api link

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {zoom: 10, center: {lat:41.900626660924985, lng:-87.62586197929068}, });
+    var map = new google.maps.Map(document.getElementById("map"), {zoom: 20, center: {lat:41.900626660924985, lng:-87.62586197929068}, });
     var marker = new google.maps.Marker({position: {lat:41.900626660924985, lng:-87.62586197929068}, map: map,});
     var infowindow = new google.maps.InfoWindow({
       content:"I love this store" });
