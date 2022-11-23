b2c5d8b3199df62308743e99e0679fc2beb34d4f
sophiamrobles <srobles2@hawk.iit.edu>
2022-11-22 23:14:41 -0500

    rename??

--- js/map.js
+++ js/map.js
@@ -1,9 +1,8 @@
 function initMap() {
-
-    var coordinates = {lat:41.900626660924985, lng:-87.62586197929068};
+    var cocochanel = {lat:41.900626660924985, lng:-87.62586197929068};
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom:10, center: coordinates});
-    var marker = new google.maps.Marker({position: coordinates, map:map});
+        document.getElementById('map'), {zoom:10, center: cocochanel});
+    var marker = new google.maps.Marker({position: cocochanel, map:map,});
 }
 window.initMap = initMap;
 
