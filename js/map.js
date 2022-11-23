63c4a0b6ebe345494579b63ce7e29e19ab9017c9
sophiamrobles <srobles2@hawk.iit.edu>
2022-11-22 23:22:51 -0500

    update

--- js/map.js
+++ js/map.js
@@ -1,8 +1,7 @@
 function initMap() {
-    var cocochanel = {lat:41.900626660924985, lng:-87.62586197929068};
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom:10, center: cocochanel});
-    var marker = new google.maps.Marker({position: cocochanel, map:map,});
+        document.getElementById('map'), {center: {lat:41.900626660924985, lng:-87.62586197929068}, zoom:10,});
+    var marker = new google.maps.Marker({position: {lat:41.900626660924985, lng:-87.62586197929068}, map:map,});
     var infowindow = new google.maps.InfoWindow({
         content:"I love this store"});
     infowindow.open(map, marker);
